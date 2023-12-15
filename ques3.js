62ad58b57e3b5e5b97a69d8761f3737bafddc004
nitlang01 <anitaolang005@gmail.com>
2023-12-15 03:42:01 -0800

    first commit

--- ques3.js
+++ ques3.js
@@ -3,9 +3,14 @@ const data = [1,3,5,6,"hello", "guava", "hello world", 34,56 ,54]
   const numberArry = [453,656,67645,5476,798,5678,989,34545,6465768,79,76545364,75,8789,987,64765,]
 
 //3(i)
+//ASCENDING ORDER
+function arrSort(arr) {
+  arr.sort(function(a,b) {  return a - b});
+}
+console.log(arrSort(numberArry))
+console.log(arrSort(data))
 
-console.log(data.sort(function(a,b) {return a-b}))
-
+//DESCENDING ORDER
 function arrSort(arr) {
   arr.sort(function(a,b) {  return b - a});
 }
--- ques5.js
+++ ques5.js
@@ -1,13 +1,18 @@
 let arr = [
-    11  3  6
-    9   5   8
-    10  7 -12
+    11,  3,  6,
+    9,   5,   8,
+    10,  7, -12,
 ]
 
-function arrDiagonalDifference(arr) { //this is the function, remember?
-    // Write your code here
+function arrDiagonalDifference(arr) {
+    let n = 0;
+    let leftdiagonal = (arr[n] + arr[n + 4] + arr[n + 8])
 
+    let m = 2
+    let rightdiagonal = (arr[n] + arr[n + 2] + arr[n + 4])
+    
+    console.log(leftdiagonal - rightdiagonal)
 }
 
 
-console.log(arrDiagonalDifference(arr)) //im calling the function here for you to see you result
+console.log(arrDiagonalDifference(arr))
